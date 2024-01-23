9e47098f239e51854bad00719fca9266c75a2a8a
Souvik Dutta <lookingjobs.2023@gmail.com>
2024-01-23 20:32:57 +0530

    commit 6

--- force-app/main/default/lwc/run_query/run_query.js
+++ force-app/main/default/lwc/run_query/run_query.js
@@ -1,14 +1,28 @@
 import { LightningElement,wire } from 'lwc';
 import sampleMC from '@salesforce/messageChannel/myMessageChannel__c';
 import {subscribe,MessageContext} from 'lightning/messageService';
+import fetchResult from '@salesforce/apex/FetchObjects.fetchResult';
 export default class Run_query extends LightningElement 
 {
     actualQuery='';
-    queryNeedToSend='';
     @wire(MessageContext)context;
     connectedCallback()
     {
-        subscribe(this.context,sampleMC,msg=>{this.actualQuery=msg.lmsData.data});
-        this.queryNeedToSend=this.actualQuery.replaceAll('\'','\\\'');
+        subscribe(this.context,sampleMC,msg=>{
+            this.actualQuery=msg.lmsData.data;
+        });
+    }
+    @wire(fetchResult,{query:'$actualQuery'})
+    dataHandler({data,error})
+    {
+        if(data)
+        {
+            console.log(JSON.stringify(data));
+        }
+        if(error)
+        {
+            console.log('Error occur');
+            console.log(error);
+        }
     }
 }
