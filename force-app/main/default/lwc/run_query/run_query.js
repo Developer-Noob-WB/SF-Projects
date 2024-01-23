750ac69581df97ea7b915dd41bdb41274e7a2e23
Souvik Dutta <lookingjobs.2023@gmail.com>
2024-01-23 19:52:21 +0530

    commit 5

--- force-app/main/default/lwc/run_query/run_query.js
+++ force-app/main/default/lwc/run_query/run_query.js
@@ -4,9 +4,11 @@ import {subscribe,MessageContext} from 'lightning/messageService';
 export default class Run_query extends LightningElement 
 {
     actualQuery='';
+    queryNeedToSend='';
     @wire(MessageContext)context;
     connectedCallback()
     {
         subscribe(this.context,sampleMC,msg=>{this.actualQuery=msg.lmsData.data});
+        this.queryNeedToSend=this.actualQuery.replaceAll('\'','\\\'');
     }
 }
